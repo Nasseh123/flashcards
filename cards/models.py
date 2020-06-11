c9bf3eb797b6846ad72702925858d58db5ad5db7
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-06-11 11:55:27 +0300

    Created the models

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Profile (models.Model):
+    profile_pic=models.ImageField(upload_to='profile/',blank=True)
+    bio=models.CharField(max_length=150,blank=True)
+
+class Subjects(models.Model):
+    subjects=models.CharField(max_length=50)
+    
+class cards(models.Model):
+    title=models.CharField(max_length=50)
+    body=models.TextField()
+    pub_date=models.DateTimeField(auto_now_add=True)
+    update_date=models.DateTimeField(auto_now_add=True)
+    subject=models.ForeignKey(Subjects,on_delete=models.CASCADE)
